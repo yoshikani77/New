6656b87dd81a44922d0746f975bb895308df89e6
yoshikani77 <yoshikani77@gmail.com>
2017-11-07 03:52:16 +0700

    Add files via upload

---
+++ New-master/Game1/Game1/States/GameState.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+using Game1.Sprites;
+
+namespace Game1.States
+{
+    public class GameState : State
+
+    {
+        GraphicsDeviceManager graphics;
+        SpriteBatch spriteBatch;
+        private List<Sprite> _sprites;
+
+        public GameState(Game1 game, GraphicsDevice graphicsDevice, ContentManager content) : base(game, graphicsDevice, content)
+        {
+
+        }
+
+        public override void Draw(GameTime gameTime, SpriteBatch spriteBatch)
+        {
+
+            spriteBatch.Begin();
+
+            foreach (var sprite in _sprites)
+                sprite.Draw(spriteBatch);
+
+            spriteBatch.End();
+            
+
+        }
+       
+
+        public override void PostUpdate(GameTime gameTime)
+        {
+            
+        }
+
+        public override void Update(GameTime gameTime)
+        {
+            foreach (var sprite in _sprites)
+                sprite.Update(gameTime, _sprites);
+
+        }
+    }
+}
---
+++ New-master/Game1/Game1/States/State.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Game1.States
+{
+    public abstract class State
+    {
+        #region Fields
+
+        protected ContentManager _content;
+
+        protected GraphicsDevice _graphicsDevice;
+
+        protected Game1 _game;
+        private Game game;
+        private GraphicsDevice graphicsDevice;
+        private ContentManager content;
+
+        #endregion
+
+        #region Methods
+        public abstract void Draw(GameTime gamTtime, SpriteBatch spriteBatch);
+
+        public abstract void PostUpdate(GameTime gameTime);
+
+        public abstract void Update(GameTime gameTime);
+
+        public State(Game1 game, GraphicsDevice graphicsDevice, ContentManager content)
+        {
+            _game = game;
+            _graphicsDevice = graphicsDevice;
+            _content = content;
+
+
+        }
+
+        public State(Game game, GraphicsDevice graphicsDevice, ContentManager content)
+        {
+            this.game = game;
+            this.graphicsDevice = graphicsDevice;
+            this.content = content;
+        }
+#endregion
+    }
+}
